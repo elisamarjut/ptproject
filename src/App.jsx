1129f1c6e1516233d2a0923e7c6e4256be6311a7
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-12-04 09:50:03 +0200

    Added AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { Link, Outlet } from 'react-router-dom'
 
 import './App.css'
+import { AppBar, Typography } from '@mui/material'
 
 function App() {
 
   return (
     <div className='App'>
-      <nav>
-        <Link to={"/"}>Home</Link><br />
-        <Link to={"/customerlist"}>Customer list</Link><br />
-        <Link to={"/traininglist"}>Training list</Link>
-      </nav>
-      <Outlet />
+      <AppBar position='absolute' color='transparent'>
+        <Typography variant='h6'>Personal trainer</Typography>
+        <nav>
+          <Link to={"/"}>Home</Link><br />
+          <Link to={"/customerlist"}>Customer list</Link><br />
+          <Link to={"/traininglist"}>Training list</Link><br />
+          <Link to={"/calendar"}>Training calendar</Link><br />
+          <Link to={"/chart"}>Training chart</Link>
+        </nav>
+        <Outlet />
+      </AppBar>
+
     </div>
   )
 }
