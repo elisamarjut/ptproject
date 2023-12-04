94247cb7aea18dd5aeb1c07d6bce389049b44cad
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-12-04 09:51:42 +0200

    Added TrainingCalendar and TrainingChart to Router

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import CustomerList from './components/Customer/CustomerList.jsx'
 import TrainingList from './components/Training/TrainingList.jsx'
 import Home from './components/Home.jsx'
 import './index.css'
+import TrainingCalendar from './components/Training/TrainingCalendar.jsx'
+import TrainingChart from './components/Training/TrainingChart.jsx'
 
 const router = createBrowserRouter([
   {
@@ -24,6 +26,14 @@ const router = createBrowserRouter([
       {
         path: "traininglist",
         element: <TrainingList />
+      },
+      {
+        path: "calendar",
+        element: <TrainingCalendar />
+      },
+      {
+        path: "chart",
+        element: <TrainingChart />
       }
     ]
   }
